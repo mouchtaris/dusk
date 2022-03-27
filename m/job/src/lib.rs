ba49f5bd0a2e80833b1889c094472a999b6e406d
Nikos Mouchtaris <mouchtaris@gmail.com>
2022-03-27 16:45:33 +0000

    Write from buffers

--- m/job/src/lib.rs
+++ m/job/src/lib.rs
@@ -1,10 +1,10 @@
 pub const VERSION: &str = "0.0.1";
 
 use {
-    error::{te, temg},
+    error::{ldebug, te, temg},
     std::{
         io, mem,
-        process::{Stdio, Child, Command, ExitStatus},
+        process::{Child, Command, ExitStatus, Stdio},
         thread::JoinHandle,
     },
 };
@@ -78,19 +78,6 @@ impl Job {
         })
     }
 
-    pub fn into_system(self) -> Result<System> {
-        Ok(match self {
-            Self::System(sys) => sys,
-            other => temg!("{:?}", other),
-        })
-    }
-    pub fn as_system_mut(&mut self) -> Result<&mut System> {
-        Ok(match self {
-            Self::System(sys) => sys,
-            other => temg!("{:?}", other),
-        })
-    }
-
     pub fn as_buffer_mut(&mut self) -> Result<&mut Buffer> {
         Ok(match self {
             Self::Buffer(buf) => buf,
@@ -122,6 +109,7 @@ impl Job {
         Ok(match self {
             Self::Spec(spec) => te!(spawn_spec(spec, capture)),
             Self::System(s) => s,
+            Self::Buffer(buf) => te!(echo_buffer(buf, capture)),
             other => panic!("{:?}", other),
         })
     }
@@ -129,31 +117,15 @@ impl Job {
         Ok(*self = Job::System(te!(mem::take(self).into_pipe(capture))))
     }
 
-    //pub fn into_buffer(self) -> Result<Buffer> {
-    //    Ok(
-    //        match self {
-    //            Self::Spec(s)
-    //        }
-    //    )
-    //}
+    pub fn into_buffer(self) -> Result<Buffer> {
+        Ok(match self {
+            Self::Spec(s) => te!(collect_output(te!(spawn_spec(s, true)))),
+            other => panic!("{:?}", other),
+        })
+    }
 
-    pub fn make_buffer(&mut self) -> Result<&[u8]> {
-        let System {
-            cmd,
-            child,
-            cleanup,
-        } = te!(mem::take(self).into_system());
-
-        let mut output = te!(child.wait_with_output());
-
-        te!(check_exit_status(&cmd, output.status));
-        te!(Cleanup::all(cleanup));
-
-        let stdout = mem::take(&mut output.stdout);
-
-        *self = Job::Buffer(Buffer::Bytes(cmd, stdout));
-
-        Ok(te!(self.as_bytes()))
+    pub fn make_buffer(&mut self) -> Result<()> {
+        Ok(*self = te!(mem::take(self).into_buffer()).into())
     }
 
     pub fn make_string(&mut self) -> Result<&str> {
@@ -164,33 +136,34 @@ impl Job {
         self.make_pipe(false)
     }
     pub fn collect(&mut self) -> Result<()> {
-        todo!()
+        self.make_buffer()
     }
     pub fn pipe(&mut self) -> Result<()> {
         self.make_pipe(true)
     }
 }
 
-fn connect_input(cmd: &mut Command, input: Job, capture: bool) -> Result<System> {
-    let mut inp_sys = te!(input.into_pipe(capture));
+fn echo_buffer(buf: Buffer, capture: bool) -> Result<System> {
+    // TODO internal piping
+    let mut cmd = Command::new("cat");
 
-    let inp_stdout = te!(inp_sys.child.stdout.take());
-    cmd.stdin(inp_stdout);
-
-    Ok(inp_sys)
-}
-fn spawn_spec(Spec { mut cmd, input }: Spec, capture: bool) -> Result<System> {
-    let mut cleanup: Vec<Cleanup> = <_>::default();
-
-    for input in input {
-        let inp_sys = te!(connect_input(&mut cmd, input, capture));
-        let inp_cleanup: Vec<Cleanup> = inp_sys.into();
-        cleanup.extend(inp_cleanup);
+    if capture {
+        cmd.stdout(Stdio::piped());
+    } else {
+        cmd.stdout(Stdio::inherit());
     }
+    cmd.stdin(Stdio::piped());
 
-    cmd.stdout(Stdio::piped());
+    let mut child = te!(cmd.spawn(), "Spawning {:?}", cmd);
+    let mut stdin = te!(child.stdin.take());
+    let bytes = buf.take_bytes();
 
-    let child = te!(cmd.spawn(), "Spawning command {:?}", cmd);
+    let thread = std::thread::spawn(move || -> Result<()> {
+        te!(io::Write::write_all(&mut stdin, &bytes));
+        Ok(())
+    });
+
+    let cleanup = vec![Cleanup::Thread(thread)];
 
     let sys = System {
         cmd,
@@ -198,15 +171,72 @@ fn spawn_spec(Spec { mut cmd, input }: Spec, capture: bool) -> Result<System> {
         cleanup,
     };
 
+    ldebug!(
+        "Echo {}buffer: {:?}",
+        if capture { "capturing " } else { "" },
+        sys
+    );
     Ok(sys)
 }
-fn spawn(mut cmd: Command) -> Result<System> {
-    let child = te!(cmd.spawn(), "Spawning command {:?}", cmd);
+
+fn collect_output(sys: System) -> Result<Buffer> {
+    let System {
+        cmd,
+        child,
+        cleanup,
+    } = sys;
+
+    let mut output = te!(child.wait_with_output());
+
+    te!(check_exit_status(&cmd, output.status));
+    te!(Cleanup::all(cleanup));
+
+    let stdout = mem::take(&mut output.stdout);
+
+    let buffer = Buffer::Bytes(cmd, stdout);
+
+    ldebug!("Collect output: {:?}", buffer);
+    Ok(buffer)
+}
+
+fn connect_input(cmd: &mut Command, input: Job) -> Result<System> {
+    let mut inp_sys = te!(input.into_pipe(true));
+
+    let inp_stdout = te!(inp_sys.child.stdout.take(), "Missing output of {:?}", cmd);
+    cmd.stdin(inp_stdout);
+
+    ldebug!("Connect input: {:?}", inp_sys);
+    Ok(inp_sys)
+}
+fn spawn_spec(Spec { mut cmd, input }: Spec, capture: bool) -> Result<System> {
+    let mut cleanup: Vec<Cleanup> = <_>::default();
+
+    for input in input {
+        let inp_sys = te!(connect_input(&mut cmd, input));
+        let inp_cleanup: Vec<Cleanup> = inp_sys.into();
+        cleanup.extend(inp_cleanup);
+    }
+
+    //cmd.stdin(Stdio::piped());
+    if capture {
+        cmd.stdout(Stdio::piped());
+    } else {
+        cmd.stdout(Stdio::inherit());
+    }
+
+    let child = te!(cmd.spawn(), "Spawning {:?}", cmd);
+
     let sys = System {
         cmd,
         child,
-        cleanup: <_>::default(),
+        cleanup,
     };
+
+    ldebug!(
+        "Spawn {}capturing command {:?}",
+        if capture { "" } else { "non-" },
+        sys
+    );
     Ok(sys)
 }
 
@@ -251,6 +281,13 @@ impl Buffer {
             Self::Bytes(_, v) => v,
             Self::String(_, v) => v.as_bytes(),
             Self::Null => &[],
+        }
+    }
+    pub fn take_bytes(self) -> Vec<u8> {
+        match self {
+            Self::Bytes(_, v) => v,
+            Self::String(_, v) => v.into_bytes(),
+            other => panic!("{:?}", other),
         }
     }
     pub fn as_str(&self) -> Result<&str> {
