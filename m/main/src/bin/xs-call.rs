3db30997074a62cdf1006c71f58a0b99216644a8
Nikos <mouchtaris@gmail.com>
2023-05-14 08:28:27 +0000

    Add --if-source-newer --than-target to xs-call

--- m/main/src/bin/xs-call.rs
+++ m/main/src/bin/xs-call.rs
@@ -7,7 +7,10 @@ fn main() {
 fn xs_call() -> Result<()> {
     te!(main::init());
 
-    let mut args = std::env::args().skip(1).collect::<Vec<_>>();
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
+
+    let mut args = te!(if_source_newer_than_target(args));
+
     args.reverse();
 
     let mut module_path = te!(args.pop(), "Missing module path");
@@ -28,4 +31,44 @@ fn xs_call() -> Result<()> {
     te!(main::make_vm_call(&mut vm, &compl, &func_addr, args)
         .map_err(|err| err.with_comment(format!("Loading icode from {}", module_path))));
     Ok(())
+}
+
+fn last_mod(path: impl AsRef<std::path::Path>) -> Result<std::time::SystemTime> {
+    Ok(te!(te!(std::fs::metadata(path)).modified()))
+}
+
+fn if_source_newer_than_target(mut args: Vec<String>) -> Result<Vec<String>> {
+    let iter = args.iter().enumerate();
+
+    const PATS: &[&str] = &["--if-source-newer=", "--than-target="];
+
+    let now = std::time::SystemTime::now();
+    let mut mods = [now, now];
+    let mut idxs = [0, 0];
+
+    for (i, arg) in iter {
+        for (j, pat) in PATS.iter().copied().enumerate() {
+            if let Some(path) = arg.strip_prefix(pat) {
+                mods[j] = te!(last_mod(path));
+                idxs[j] = i;
+            }
+        }
+    }
+
+    let [source_mod, target_mod] = mods;
+
+    match () {
+        _ if target_mod == source_mod => {}
+        _ if target_mod > source_mod => {
+            std::process::exit(0);
+        }
+        _ => {
+            idxs.sort();
+            let [j, i] = idxs;
+            args.swap_remove(i);
+            args.swap_remove(j);
+        }
+    }
+
+    Ok(args)
 }
