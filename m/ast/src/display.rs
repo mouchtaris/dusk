953089a97038099562022333a8af54dd4abb2057
Nikos <mouchtaris@protonmail.com>
2024-12-04 07:44:25 +0000

    Display more ast

--- m/ast/src/display.rs
+++ m/ast/src/display.rs
@@ -35,3 +35,112 @@ impl<'i> AsRef<std::path::Path> for Path<'i> {
         }
     }
 }
+
+impl<'i> fmt::Display for Variable<'i> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Self((id,)) = self;
+        write!(f, "${id}")
+    }
+}
+
+impl<'i> fmt::Display for Dereference<'i> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Self((id,)) = self;
+        write!(f, "*{id}")
+    }
+}
+
+impl<'i> fmt::Display for Invocation<'i> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Self((comment, target, cwd, inp, out, env, args)) = self;
+        for line in comment {
+            writeln!(f, "{line}")?;
+        }
+        write!(f, "{target}")?;
+        if let Some(cwd) = cwd {
+            write!(f, " {cwd}")?;
+        }
+        for RedirectInput((redir,)) in inp {
+            write!(f, " <{redir}")?;
+        }
+        for RedirectOutput((redir,)) in out {
+            write!(f, " >{redir}")?;
+        }
+        for (name, val) in env {
+            write!(f, "i {name}={val}")?;
+        }
+        for arg in args {
+            write!(f, " {arg}")?;
+        }
+        Ok(())
+    }
+}
+impl<'i> fmt::Display for InvocationCwd<'i> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use InvocationCwd::*;
+        match self {
+            Path(path) => write!(f, "{path}")?,
+            Variable(variable) => write!(f, "{variable}")?,
+            BoxInvocation(invocation) => write!(f, "{invocation}")?,
+        }
+        Ok(())
+    }
+}
+impl<'i> fmt::Display for Redirect<'i> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Redirect::*;
+        match self {
+            Path(path) => write!(f, "{path}")?,
+            Variable(variable) => write!(f, "{variable}")?,
+            Dereference(dereference) => write!(f, "{dereference}")?,
+            Invocation(invocation) => write!(f, "{invocation}")?,
+            Slice(slice) => write!(f, "{slice}")?,
+            String(super::String((s,))) => write!(f, "{s}")?,
+        }
+        Ok(())
+    }
+}
+impl<'i> fmt::Display for InvocationArg<'i> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use InvocationArg::*;
+        match self {
+            Ident(s) => write!(f, "{s}")?,
+            Opt(opt) => write!(f, "{opt}")?,
+            Path(path) => write!(f, "{path}")?,
+            String(super::String((s,))) => write!(f, "{s}")?,
+            Variable(variable) => write!(f, "{variable}")?,
+            Word(super::Word((s,))) => write!(f, "{s}")?,
+            Natural(super::Natural((s,))) => write!(f, "{s}")?,
+            Invocation(invocation) => write!(f, "{invocation}")?,
+            Slice(slice) => write!(f, "{slice}")?,
+        }
+        Ok(())
+    }
+}
+impl<'i> fmt::Display for Slice<'i> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Self((name, range)) = self;
+        write!(f, "{name}{range}")?;
+        Ok(())
+    }
+}
+impl<'i> fmt::Display for Opt<'i> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Opt::*;
+        match self {
+            ShortOpt(super::ShortOpt((s,))) => write!(f, "{s}")?,
+            LongOpt(super::LongOpt((s,))) => write!(f, "{s}")?,
+        }
+        Ok(())
+    }
+}
+impl<'i> fmt::Display for Range<'i> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Range::*;
+        match self {
+            DoubleRange((a, b)) => write!(f, "[{a}; {b}]")?,
+            Index(invocation_arg) => write!(f, "[{invocation_arg}]")?,
+        }
+        Ok(())
+    }
+}
